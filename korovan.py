7fed1242deaf4136e48fa95300a61b5047bffd81
Govorun11 <edik0076207@gmail.com>
2020-12-19 14:43:15 +0200

    'abc'

---
+++ korovan.py
@@ -0,0 +1,10 @@
+
+
+
+h = 'hello '
+w = "world"
+i = 'I am studying python!'
+s = h + w;                                 '''Объединение 2-ух строк в 1'''
+print(s.upper(), i, sep='!\n', end='=)');  '''Вывод текста на экран'''
+
+#gdsgbsgdjbsjkgk
