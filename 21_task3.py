94af829a4286f9f40244518a30fa02cc2031112a
Govorun11 <edik0076207@gmail.com>
2020-12-23 12:24:31 +0200

    absv

--- 21_task3.py
+++ 21_task3.py
@@ -1,13 +1,11 @@
 """когда my_name == name, не работает принтует почему то,
 но когда my_name != name, принт работает(((( """
 
-
-my_name = 'Edik'
-name = input('Введите ваше имя:\n')
-if name.lower().find('edik'):
-    if my_name == name:
-        print('Добро пожаловать, !'+my_name)
-    else:
+name = 'edik'
+my_name = input('Введите ваше имя:\n')
+if my_name.lower()in name:
+    print('Добро пожаловать, '+my_name)
+else:
         print('Ты не мой хозяин')
 
 
